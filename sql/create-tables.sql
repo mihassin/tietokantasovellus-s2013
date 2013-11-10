1f32af11545d66a6deb6ea6b6efef31fd629a36d
mihassin <marko.hassinen@helsinki.fi>
2013-11-10 15:54:40 +0200

    sql

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE product_types (
 
 CREATE TABLE products (
   id serial PRIMARY KEY,
-  product_type_id int REFERENCES product_type(id) ON DELETE CASCADE,
+  product_type_id int REFERENCES product_types(id) ON DELETE CASCADE,
   name varchar(30) NOT NULL,
   description varchar(255),
   price decimal(6,2)
@@ -18,16 +18,16 @@ CREATE TABLE role_types (
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
-  role_type_id int REFERENCES role_type(id) ON DELETE CASCADE,
+  role_type_id int REFERENCES role_types(id) ON DELETE CASCADE,
   first varchar(30) NOT NULL,
   second varchar(30) NOT NULL,
-  e-mail varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
   phone varchar(255) NOT NULL
 );
 
 CREATE TABLE orders (
   id serial PRIMARY KEY,
-  user_id int REFERENCES user(id) ON DELETE CASCADE,
+  user_id int REFERENCES users(id) ON DELETE CASCADE,
   address varchar(255) NOT NULL,
   deliver_time date NOT NULL,
   total_price decimal(6,2)
@@ -35,22 +35,22 @@ CREATE TABLE orders (
 
 CREATE TABLE materials (
   id serial PRIMARY KEY,
-  product_type_id int REFERENCES product_type(id) ON DELETE CASCADE,
+  product_type_id int REFERENCES product_types(id) ON DELETE CASCADE,
   description varchar(30) NOT NULL,
   price decimal(6,2)
 );
 
 CREATE TABLE content_map (
   id serial PRIMARY KEY,
-  product_id int REFERENCES product(id) ON DELETE CASCADE,
-  material_id int REFERENCES material(id) ON DELETE CASCADE,
+  product_id int REFERENCES products(id) ON DELETE CASCADE,
+  material_id int REFERENCES materials(id) ON DELETE CASCADE,
   UNIQUE(product_id, material_id)
 );
 
 CREATE TABLE cart_map (
   id serial PRIMARY KEY,
-  order_id int REFERENCES order(id) ON DELETE CASCADE,
-  product_id int REFERENCES product(id) ON DELETE CASCADE,
+  order_id int REFERENCES orders(id) ON DELETE CASCADE,
+  product_id int REFERENCES products(id) ON DELETE CASCADE,
   amount int NOT NULL,
   UNIQUE(order_id, product_id)
 );

065db9a1c19c240d0b6a1997463433be4e8c1046
mihassin <marko.hassinen@helsinki.fi>
2013-11-10 15:40:15 +0200

    sql

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE product_type (
 
 CREATE TABLE product (
   id serial PRIMARY KEY,
-  product_type_id REFERENCES product_type(id) ON DELETE CASCADE,
+  product_type_id int REFERENCES product_type(id) ON DELETE CASCADE,
   name varchar(30) NOT NULL,
   description varchar(255),
   price decimal(6,2)
@@ -18,7 +18,7 @@ CREATE TABLE role_type (
 
 CREATE TABLE user (
   id serial PRIMARY KEY,
-  role_type_id REFERENCES role_type(id) ON DELETE CASCADE,
+  role_type_id int REFERENCES role_type(id) ON DELETE CASCADE,
   first varchar(30) NOT NULL,
   second varchar(30) NOT NULL,
   e-mail varchar(255) NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE user (
 
 CREATE TABLE order (
   id serial PRIMARY KEY,
-  user_id REFERENCES user(id) ON DELETE CASCADE,
+  user_id int REFERENCES user(id) ON DELETE CASCADE,
   address varchar(255) NOT NULL,
   deliver_time date NOT NULL,
   total_price decimal(6,2)
@@ -35,18 +35,22 @@ CREATE TABLE order (
 
 CREATE TABLE material (
   id serial PRIMARY KEY,
-  product_type_id REFERENCES product_type(id) ON DELETE CASCADE,
+  product_type_id int REFERENCES product_type(id) ON DELETE CASCADE,
   description varchar(30) NOT NULL,
   price decimal(6,2)
 );
 
---CREATE TABLE content (
-  --product_id REFERENCES product(id) ON DELETE CASCADE,
-  --material_id REFERENCES material(id) ON DELETE CASCADE
---);
+CREATE TABLE content (
+  id serial PRIMARY KEY,
+  product_id int REFERENCES product(id) ON DELETE CASCADE,
+  material_id int REFERENCES material(id) ON DELETE CASCADE,
+  UNIQUE(product_id, material_id)
+);
 
---CREATE TABLE CART(
-  --order_id REFERENCES ORDER(order_id),
-  --product_id REFERENCES PRODUCT(product_id),
-  --amount integer NOT NULL
---);
+CREATE TABLE cart (
+  id serial PRIMARY KEY,
+  order_id int REFERENCES order(id) ON DELETE CASCADE,
+  product_id int REFERENCES product(id) ON DELETE CASCADE,
+  amount int NOT NULL,
+  UNIQUE(order_id, product_id)
+);

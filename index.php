2307b8192f1bd70bbec75ffbdf91fad8f54ecf57
Marko I Hassinen <mihassin@svm-19.cs.helsinki.fi>
2013-11-15 15:23:39 +0200

    etusivu

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head><title>Pizzapalvelu</title></head>
+<body>
+<nav>
+<a href="index.php">Etusivu</a>
+<a href="views/login.php">Kirjaudu sisään</a>
+<a href="views/newuser.php">Luo tunnus</a>
+</nav></br>
+<h1>Pizzapalvelu</h1>
+<p>Tervetuloa käyttämään pizzapalvelua!</p>
+<p>Aloita luomalla tunnus tai kirjautumalla sisään</p>
+<ul>
+	<li><a href="views/menu.php">Ruokalista</a></li>
+	<li><a href="views/drinks.php">Juomat</a></li>
+	<li><a href="views/materials.php">Lisukkeet</a></li>
+</ul>
+<footer>
+<time>Viimeksi muokattu 2013.11.15 14:19</time>
+</footer>
+</body>
+</html>

2536df80703fbd3373cf3840c38dfa6b991e35e3
mihassin <marko.hassinen@helsinki.fi>
2013-11-15 14:03:52 +0200

    mvc

--- views/newuser.php
+++ views/newuser.php
@@ -1,12 +1,12 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Kirjaudu sisään</title>
+	<title>Uusi käyttäjä</title>
 </head>
 <body>
-	<h1>Kirjaudu sisään</h1>
+	<h1>Luo käyttäjätunnus</h1>
 	<div>
-	<form>
+	<form method="post">
 		<label>Sähköposti</label></br>
 		<input type="email" name="email" id="email" /></br>
 		<label>Salasana</label></br>
@@ -16,7 +16,9 @@
 		<label>Sukunimi</label></br>
 		<input type="text" name="second" id="second" /></br>		
 		<label>Puhelin numero</label></br>
-		<input type="text" name="phone" id="phone" /></br>	
+		<input type="text" name="phone" id="phone" /></br></br>	
+		<input type="submit" name="luotunnus-nappi" id="luotunnus-nappi" value="Luo tunnus"/>	
+		<input type="submit" name="kirjaudu-sisään" id="kirjaudu-sisään" formaction="login.php" value="Kirjaudu sisään"/>	
 	</form>
 	</div>
 </body>

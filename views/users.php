6bf1cb9a56803bf85fbda20c5700dcb79a5ebdab
mihassin <marko.hassinen@helsinki.fi>
2013-11-15 17:42:18 +0200

    lisää

--- views/users.php
+++ views/users.php
@@ -7,6 +7,9 @@
     </head>
     <body>
         <h1>Käyttäjät</h1>
-	<?php require_once __DIR__.'/../libs/listing.php'; ?>
+	<?php 
+		require_once __DIR__.'/../libs/listing.php'; 
+		getList($kysely);
+	?>
     </body>
 </html>

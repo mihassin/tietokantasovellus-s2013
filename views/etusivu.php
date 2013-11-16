7d69fc66cb1af2a305b636103834f65df5d610ca
mihassin <marko.hassinen@helsinki.fi>
2013-11-16 16:21:12 +0200

    lauantai

--- views/etusivu.php
+++ views/etusivu.php
@@ -3,8 +3,8 @@ echo '<h1>Pizzapalvelu</h1>
 <p>Tervetuloa käyttämään pizzapalvelua!</p>
 <p>Aloita luomalla tunnus tai kirjautumalla sisään</p>
 <ul>
-<li><a href="views/menu.php">Ruokalista</a></li>
-<li><a href="views/drinks.php">Juomat</a></li>
-<li><a href="views/materials.php">Lisukkeet</a></li>
+<li><a href="menu.php">Ruokalista</a></li>
+<li><a href="drinks.php">Juomat</a></li>
+<li><a href="materials.php">Lisukkeet</a></li>
 </ul>';
 ?>
--- views/menu.php
+++ views/menu.php
@@ -1,15 +1,1 @@
-<?php $kysely = "SELECT name, price FROM products WHERE product_type_id='1'"; ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Käyttäjät</title>
-    </head>
-    <body>
-        <h1>Ruokalista</h1>
-	<?php 
-		require_once __DIR__.'/../libs/listing.php'; 
-		getList($kysely);
-	?>
-    </body>
-</html>
+<?php echo "<h1>Ruokalista</h1>"; ?>

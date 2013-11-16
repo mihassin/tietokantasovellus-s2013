a5fba91aa961c83c8ed5f22fdf1acf23b626974d
mihassin <marko.hassinen@helsinki.fi>
2013-11-16 16:25:01 +0200

    lauantai

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <?php $kysely = "SELECT name, price FROM products WHERE product_type_id='1'"; 
-      require_once 'header.php';
-      require_once '/views/menu.php'; 
-      require_once '/libs/listing.php'; 
+      require_once 'views/header.php';
+      require_once 'views/menu.php'; 
+      require_once 'libs/listing.php'; 
       getList($kysely);
-      require_once 'footer.php'; 
+      require_once 'views/footer.php'; 
 ?>

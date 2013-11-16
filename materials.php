b85028c577d8f998209d44c5b45b85a7dce920fe
mihassin <marko.hassinen@helsinki.fi>
2013-11-16 18:05:10 +0200

    lauantai

--- materials.php
+++ materials.php
@@ -1,6 +1,6 @@
 <?php $kysely = "SELECT description, price FROM materials"; 
       require_once 'views/header.php';
-      require_once 'views/menu.php'; 
+      require_once 'views/materials.php'; 
       require_once 'libs/listing.php'; 
       getList($kysely);
       require_once 'views/footer.php'; 

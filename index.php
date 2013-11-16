cc0ddcecc711caccf77459e13621da6f262d2d4f
mihassin <marko.hassinen@helsinki.fi>
2013-11-16 12:43:49 +0200

    etusivu

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE HTML>
-<html>
-<?php require_once 'views/header.php'; ?>
-<h1>Pizzapalvelu</h1>
-<p>Tervetuloa käyttämään pizzapalvelua!</p>
-<p>Aloita luomalla tunnus tai kirjautumalla sisään</p>
-<ul>
-	<li><a href="views/menu.php">Ruokalista</a></li>
-	<li><a href="views/drinks.php">Juomat</a></li>
-	<li><a href="views/materials.php">Lisukkeet</a></li>
-</ul>
-<?php require_once 'views/footer.php'; ?>
-</html>
+<?php require_once 'views/header.php';
+require_once 'views/etusivu.php';
+require_once 'views/footer.php'; ?>

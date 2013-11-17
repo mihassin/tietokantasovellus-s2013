ac97dc32d9928c2912531b5c23dbcad39e493f63
mihassin <marko.hassinen@helsinki.fi>
2013-11-17 14:48:43 +0200

    sunnuntai

--- newuserCheck.php
+++ newuserCheck.php
@@ -1,37 +1,34 @@
 <?php
-
+require_once 'libs/db_connect.php';
 $registerOkay = TRUE;
 $emailLegit = TRUE;
 $phoneLegit = TRUE;
 $pwLongEnuf = TRUE;
 
-require_once 'libs/checkEmail.php';
-
-if(strlen($_POST['email'] < 5) {
+if( strlen($_POST['email'] < 5) ) {
 	$registerOkay = FALSE;
 	$emailLegit = FALSE;
 }
- 
+$yhteys = getConnection();
 else
 {
-	if(emailExists($_POST['email'])) {
+	$claimMail = pg_escape_string($yhteys, $_POST['email']);
+	$kysely = "SELECT id FROM users WHERE email='{$claimMail}'";
+	$res = pg_query($yhteys, $query);
+
+	if(pg_num_rows($res) > 0) {
 		$registerOkay = FALSE;
 		$emailLegit = FALSE;
 	}
 }
 
-if(strlen($_POST['phone'] < 6) {
+if( strlen($_POST['phone'] < 6) ) {
 $registerOkay = FALSE;
 $phoneLegit = FALSE;
 }
 
-if(strlen($_POST['password'] < 5) {
-$registerOkay = FALSE;
-$pwLongEnuf = FALSE;
-}
-
 require_once 'views/header.php';
-if($registerOkay == FALSE)
+if(!$registerOkay)
 {
 	if($emailLegit == FALSE) echo 'Antamasi sähköpostiosoite on jo käytössä!</br>';
 	if($phoneLegit == FALSE) echo 'Tarkista puhelinnumerosi!</br>';
@@ -40,7 +37,19 @@ if($registerOkay == FALSE)
 }
 else
 {
-addAccount($_POST['first'],$_POST['second'],$_POST['email'],$_POST['phone'],$_POST['password']);
+$role = 1;
+$first = $pg_escape_string($yhteys,$_POST['first']);
+$second = $pg_escape_string($yhteys,$_POST['second']);
+$email = $pg_escape_string($yhteys,$_POST['email']);
+$phone = $pg_escape_string($yhteys,$_POST['phone']);
+$pw = $pg_escape_string($yhteys,$_POST['password']);
+
+$kysely = "INSERT INTO users VALUES ({$id}, {$role}, '{$first}', '{$second}', '{$email}', '{$phone}', md5('{$pw}'), substring(md5(random()::TEXT) from 1 for 8));";
+$lisaa = pg_query($yhteys, $kysely);
+
+$kysely = "UPDATE users SET pw_hash=md5('{$pw}' || users.pw_salt) WHERE email='{$email}'";
+$lisaa = pg_query($yhteys, $kysely);
+
 echo "Käyttäjätunnus luotu</br>Voit kirjautua sisään antamallasi sähköpostiosoitteella!";
 }
 require_once 'views/footer.php';?>

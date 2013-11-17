5e8d1cff077727d55f4bc482d1b09efc42527310
mihassin <marko.hassinen@helsinki.fi>
2013-11-17 11:49:54 +0200

    sunnuntai

--- views/login.php
+++ views/login.php
@@ -1,13 +1,12 @@
 <?php require_once 'header.php'; ?>
 	<h1>Kirjaudu sisään</h1>
 	<div>
-	<form method="post">
+	<form method="post" action="checklogin.php">
 		<label>Käyttäjätunnus</label></br>
 		<input type="text" name="user" id="user" /></br>
 		<label>Salasana</label></br>
 		<input type="password" name="password" id="password" /></br></br>
 		<input type="submit" name="kj-nappi" id="kj-nappi" value="Kirjaudu sisään" />
-		<input type="submit" name="ua-nappi" id="ua-nappi" formaction="newuser.php" value="Luo tunnus" />
 	</form>
 	</div>
 <?php require_once 'footer.php'; ?>

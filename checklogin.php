f8da2a4a1bcbbcc3bd85aee149c82823e3907bbe
mihassin <marko.hassinen@helsinki.fi>
2013-11-17 12:16:36 +0200

    sunnuntai

--- checklogin.php
+++ checklogin.php
@@ -2,19 +2,17 @@
 session_start();
 require_once 'libs/db_connection';
 
-if((!isset($_POST['email'])) || (!isset($_POST['password']))) {
-header('Location: http://mihassin.users.cs.helsinki.fi/login.php');
-exit;
-}
-
 $yhteys = getConnection();
-$email = pg_escape_string($yhteys, $_POST['email']);
-$password = pg_escape_string($yhteys, $_POST['password']); 
+$email = $_POST['email'];
+$password = $_POST['password']; 
 
 $kysely = "SELECT id FROM users WHERE email = '{$email}' AND pw_hash = md5('{$password}' || users.pw_salt)";
-$res = pg_query($query);
+$res = pg_query($yhteys, $kysely);
 
 if(pg_num_rows($res) != 1) {
 echo "moi";
+}
+else {
+echo "hei";
 }
 ?>
--- views/login.php
+++ views/login.php
@@ -3,10 +3,10 @@
 	<div>
 	<form method="post" action="checklogin.php">
 		<label>Käyttäjätunnus</label></br>
-		<input type="text" name="email" id="email" /></br>
+		<input type="text" name="email" /></br>
 		<label>Salasana</label></br>
-		<input type="password" name="password" id="password" /></br></br>
-		<input type="submit" name="kj-nappi" id="kj-nappi" value="Kirjaudu sisään" />
+		<input type="password" name="password" /></br></br>
+		<input type="submit" name="kj-nappi" value="Kirjaudu sisään" />
 	</form>
 	</div>
 <?php require_once 'footer.php'; ?>

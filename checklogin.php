cf93f9fd50fa189491aa87d50db6edab035ac11f
mihassin <marko.hassinen@helsinki.fi>
2013-11-17 12:30:52 +0200

    sunnuntai

--- checklogin.php
+++ checklogin.php
@@ -1,18 +1,10 @@
 <?php
-session_start();
-require_once 'libs/db_connection';
+require_once 'libs/db_connect.php';
 
 $yhteys = getConnection();
-$email = $_POST['email'];
-$password = $_POST['password']; 
+$email = pg_escape_string($yhteys,$_POST['email']);
+$password = pg_escape_string($yhteys,$_POST['password']); 
 
-$kysely = "SELECT id FROM users WHERE email = '{$email}' AND pw_hash = md5('{$password}' || users.pw_salt)";
-$res = pg_query($yhteys, $kysely);
-
-if(pg_num_rows($res) != 1) {
-echo "moi";
-}
-else {
-echo "hei";
-}
+$kysely = "SELECT id FROM users WHERE email = '{$email}' AND pw_hash=md5('{$password}' || users.pw_salt)";
+echo $kysely;
 ?>

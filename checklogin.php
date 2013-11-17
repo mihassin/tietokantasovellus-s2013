6a0f7314864ad913daf343fd0a79a2d07df8e5f5
mihassin <marko.hassinen@helsinki.fi>
2013-11-17 12:33:36 +0200

    sunnuntai

--- checklogin.php
+++ checklogin.php
@@ -6,5 +6,7 @@ $email = pg_escape_string($yhteys,$_POST['email']);
 $password = pg_escape_string($yhteys,$_POST['password']); 
 
 $kysely = "SELECT id FROM users WHERE email = '{$email}' AND pw_hash=md5('{$password}' || users.pw_salt)";
-echo $kysely;
+$query = pg_query($yhteys, $kysely);
+$res = pg_num_rows($query);
+echo $res;
 ?>

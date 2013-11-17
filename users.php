63af39164731853dda8a5a24551ea41d4f95bbfd
mihassin <marko.hassinen@helsinki.fi>
2013-11-17 17:38:24 +0200

    sunnuntai

--- users.php
+++ users.php
@@ -1,7 +1,7 @@
 <?php require_once 'libs/listqueries.php';
       $kysely = getUsers(); 
       require_once 'views/header.php';
-      require_once 'views/drinks.php'; 
+      require_once 'views/users.php'; 
       require_once 'libs/listing.php'; 
       getList($kysely);
       require_once 'views/footer.php'; 

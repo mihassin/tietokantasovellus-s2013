f96fe4c8084fc0c2633275454e706758f99e5dc2
mihassin <marko.hassinen@helsinki.fi>
2013-11-17 15:21:14 +0200

    sunnuntai

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php require_once 'views/header.php';
+<?php session_start();
+if($_SESSION['userRole'] == 1)
+require_once 'views/customer-header.php';
+else 
+require_once 'views/header.php';
 require_once 'views/etusivu.php';
 require_once 'views/footer.php'; ?>

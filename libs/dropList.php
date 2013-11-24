fa96ca4bb4eab7c502f26923bb37240ae296da48
mihassin <marko.hassinen@helsinki.fi>
2013-11-24 17:49:14 +0200

    sunnuntai

---
+++ libs/dropList.php
@@ -0,0 +1,13 @@
+<?php 
+function getDropList() {
+ require_once 'db_connect.php';
+ $yhteys = getConnection();
+ $kysely = "SELECT name FROM products WHERE product_type_id='1'";
+ $kysely = pg_query($yhteys, $kysely);
+ $rows = pg_num_rows($kysely);
+
+ $res = pg_fetch_all($kysely);
+
+ for($i = 0; $i<$rows ; $i++) {
+ }
+}?>

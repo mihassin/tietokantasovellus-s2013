a85b61a80b4729ba17f5780770e5c7c76882c143
Marko I Hassinen <mihassin@svm-19.cs.helsinki.fi>
2013-11-24 17:28:32 +0200

    sunnuntai

--- views/add-offer.php
+++ views/add-offer.php
@@ -1,11 +1,13 @@
 <?php require_once 'header.php' ?>
 <h1>Lisää tarjous</h1>
+ <label>Valitse tuote</label></br>
  <select name="productlist" form="offerform">
   <option value="a">a</option>
  </select>
  <form action="add-offerCheck.php" id="offerform">
-  <label>Tarjous hinta</label>
-  <input type="text" name="price" />
+  </br>
+  <label>Tarjous hinta</label></br>
+  <input type="text" name="price" /></br>
   <input type="submit" value="Lisää tarjous" />
  </form>
 <?php require_once 'footer.php' ?>

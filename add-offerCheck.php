6ff80c31241ec40161c3125c21373f013b9cb461
mihassin <marko.hassinen@helsinki.fi>
2013-11-24 18:53:07 +0200

    sunnuntai

--- add-offerCheck.php
+++ add-offerCheck.php
@@ -1,11 +1,16 @@
-<?php 
-require_once 'libs/checkAccess.php'
+<?php
+if(!is_numeric($_POST['price'])) {
+ header('Location: http://mihassin.users.cs.helsinki.fi/add-offer.php');
+}
+ 
+require_once 'libs/checkAccess.php';
 require_once 'libs/db_connect.php';
 $yhteys = getConnection();
 $name = pg_escape_string($yhteys, $_POST['productlist']);
 $price = pg_escape_string($yhteys, $_POST['price']);
-echo $name . "</br>" . $price;
+
 $kysely = "UPDATE products SET price='{$price}' WHERE name='{$name}';";
-echo $kysely;
+
 pg_query($kysely);
+header('Location: http://mihassin.users.cs.helsinki.fi/menu.php');
 ?>

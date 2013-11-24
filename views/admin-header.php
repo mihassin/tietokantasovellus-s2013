df85268ef82cd2a9cf4f50b68e4c60e23e1ea977
mihassin <marko.hassinen@helsinki.fi>
2013-11-24 20:02:44 +0200

    sunnuntai

--- views/admin-header.php
+++ views/admin-header.php
@@ -5,6 +5,7 @@ echo "<!DOCTYPE html><html>
 <body><nav>
 <a href='index.php'>Etusivu</a>
 <a href='users.php'>Käyttäjät</a>
+<a href='orders.php'>Tilaukset</a>
 <a href='logout.php'>Kirjaudu ulos</a>
 </nav></br>";
 ?>
--- views/employee-header.php
+++ views/employee-header.php
@@ -4,7 +4,7 @@ echo "<!DOCTYPE html><html>
 <title>Pizzapalvelu</title></head>
 <body><nav>
 <a href='index.php'>Etusivu</a>
-<a href='users.php'>Käyttäjät</a>
+<a href='orders.php'>Tilaukset</a>
 <a href='logout.php'>Kirjaudu ulos</a>
 </nav></br>";
 ?>

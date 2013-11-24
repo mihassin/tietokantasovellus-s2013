d95ec9130e35913da45886f5270ae238f5184984
mihassin <marko.hassinen@helsinki.fi>
2013-11-24 16:31:34 +0200

    sunnuntai

---
+++ add-offer.php
@@ -0,0 +1,7 @@
+<?php session_start();
+
+if(isset($_SESSION['userId']) || ($_SESSION['userId'] == 1)) {
+        header('Location: http://mihassin.users.cs.helsinki.fi');
+} 
+require_once 'views/header.php' ?>
+<?php require_once 'views/footer.php' ?>

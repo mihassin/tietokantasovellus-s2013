d4a1184e1c1b8c030903e3eecde5184cefb70a36
mihassin <marko.hassinen@helsinki.fi>
2013-11-24 18:56:11 +0200

    sunnuntai

--- menu.php
+++ menu.php
@@ -3,6 +3,10 @@
       require_once 'views/header.php';
       require_once 'views/menu.php'; 
       require_once 'libs/listing.php'; 
-      getList($kysely);
+      getList($kysely); ?>
+<form action='add-offer.php'>
+ <input type="submit" value="Lisää tarjous" />
+</form>
+<?php      
       require_once 'views/footer.php'; 
 ?>

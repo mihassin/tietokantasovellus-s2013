6d1c7ae5f033e278eeb6fa287190637459dc263c
mihassin <marko.hassinen@helsinki.fi>
2013-11-24 19:47:17 +0200

    sunnuntai

--- views/add-offerButton.php
+++ views/add-offerButton.php
@@ -1,5 +1,5 @@
 <?php
-if($_SESSION['userRole'] =< 4 && $_SESSION['userRole'] > 1) {
+if(($_SESSION['userRole'] == 4) || ($_SESSION['userRole'] == 3) || ($_SESSION['userRole'] == 2)) {
 echo "<form action='add-offer.php'>
  <input type='submit' value='Lisää tarjous' />
 </form>"; 

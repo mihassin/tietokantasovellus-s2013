fe7660b6abc99f8acc13051574e3f76fd04490a1
mihassin <marko.hassinen@helsinki.fi>
2013-11-28 09:43:39 +0200

    torstai

--- views/add-offerButton.php
+++ views/add-offerButton.php
@@ -3,4 +3,18 @@ if(($_SESSION['userRole'] == 4) || ($_SESSION['userRole'] == 3) || ($_SESSION['
 echo "<form action='add-offer.php'>
  <input type='submit' value='Lisää tarjous' />
 </form>"; 
-} ?>
+} 
+
+if($_SESSION['userRole'] == 4) {
+echo "<form action='add-item.php'>
+ <input type='submit' value='Lisää tuote' />
+</form></br>
+<select name='productlist' form='removeform'>";
+  getDropList($kysely);
+echo"</select>
+<form method='post' action='remove-itemCheck.php' id='removeform'>
+  </br>
+ <input type='submit' value='Poista tuote' />
+</form>";
+}
+?>

c3553ec81ca3118397c11291d2fa127fa1bed4f7
mihassin <marko.hassinen@helsinki.fi>
2013-11-28 10:03:12 +0200

    torstai

--- views/add-offerButton.php
+++ views/add-offerButton.php
@@ -1,6 +1,8 @@
 <?php
 if($_SESSION['userRole'] == 4) {
 echo "<select name='productlist' form='removeform'>";
+  $kysely = "SELECT name FROM products WHERE product_type_id='1'";
+  require_once 'libs/dropList.php';
   getDropList($kysely);
 echo"</select>
 <form method='post' action='remove-itemCheck.php' id='removeform'>

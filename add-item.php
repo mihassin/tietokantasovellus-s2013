e10297468cbd7718ecb6ef0e99529a6893a893f3
mihassin <marko.hassinen@helsinki.fi>
2013-11-28 10:35:21 +0200

    torstai

---
+++ add-item.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'libs/checkAccess.php';
+require_once 'libs/dropList.php';
+$kysely = "SELECT description FROM product_types";
+require_once 'views/add-item.php'; ?>
---
+++ views/add-item.php
@@ -0,0 +1,17 @@
+<?php require_once 'header.php' ?>
+<h1>Lisää tuote</h1>
+ <label>Valitse tuotetyyppi</label></br>
+ <select name="product-type-list" form="addform">
+  <?php getDropList($kysely); ?>
+ </select>
+ <form method="post" action="add-itemCheck.php" id="addform">
+  </br>
+  <label>Tuotenimi</label></br>
+  <input type="text" name="name" /></br>
+  <label>Tuotekuvaus</label></br>
+  <input type="text" name="description" /></br>
+  <label>Hinta</label></br>
+  <input type="text" name="price" /></br>
+  <input type="submit" value="Lisää tarjous" />
+ </form>
+<?php require_once 'footer.php' ?>

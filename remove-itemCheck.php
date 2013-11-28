421ac682a7a88cfc0925c0979c2123a09280bfbc
mihassin <marko.hassinen@helsinki.fi>
2013-11-28 10:28:04 +0200

    torstai

--- remove-itemCheck.php
+++ remove-itemCheck.php
@@ -3,4 +3,5 @@ require_once 'libs/checkAccess.php';
 require_once 'libs/db_connect.php';
 $name = pg_escape_string(getConnection(), $_POST['productlist']);
 $sql = "DELETE FROM products WHERE product_type_id='1' AND name='{$name}';";
-pg_query(getConnection(), $sql);?>
+pg_query(getConnection(), $sql);
+header('Location: http://mihassin.users.cs.helsinki.fi/menu.php');?>

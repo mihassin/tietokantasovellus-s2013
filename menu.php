3a9c97463daa6da9b8ab1dd6d2b73c544cafb41e
mihassin <marko.hassinen@helsinki.fi>
2013-11-28 10:10:55 +0200

    torstai

--- menu.php
+++ menu.php
@@ -4,6 +4,7 @@
       require_once 'views/menu.php'; 
       require_once 'libs/listing.php'; 
       getList($kysely);
-      require_once 'views/add-offerButton.php';
+      if( ($_SESSION['userRole'] == 4) || ($_SESSION['userRole'] == 3) || ($_SESSION['userRole'] == 2) )
+       require_once 'views/add-offerButton.php';
       require_once 'views/footer.php'; 
 ?>
--- views/add-offerButton.php
+++ views/add-offerButton.php
@@ -1,10 +1,9 @@
 <?php
-if($_SESSION['userRole'] == 4) {
 echo "<select name='productlist' form='removeform'>";
   $kysely = "SELECT name FROM products WHERE product_type_id='1'";
   require_once 'libs/dropList.php';
   getDropList($kysely);
-echo"</select>
+echo "</select>
 <form method='post' action='remove-itemCheck.php' id='removeform'>
   </br>
  <input type='submit' value='Poista tuote' />
@@ -12,10 +11,6 @@ echo"</select>
 <form action='add-item.php'>
  <input type='submit' value='Lisää tuote' />
 </form>";
-}
-
-if(($_SESSION['userRole'] == 4) || ($_SESSION['userRole'] == 3) || ($_SESSION['userRole'] == 2)) {
 echo "<form action='add-offer.php'>
  <input type='submit' value='Lisää tarjous' />
-</form>"; 
-} ?>
+</form>"; ?>

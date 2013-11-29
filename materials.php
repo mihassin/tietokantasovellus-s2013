b2f65bcb539b37e2af04ec6c9afec930a6ae9095
mihassin <marko.hassinen@helsinki.fi>
2013-11-29 17:57:19 +0200

    perjantai

--- materials.php
+++ materials.php
@@ -4,6 +4,7 @@
       require_once 'views/materials.php'; 
       require_once 'libs/listing.php'; 
       getList($kysely);
+      $kysely = "SELECT name FROM products WHERE product_type_id='3';";
       $_SESSION['pid'] = 3;
       if( ($_SESSION['userRole'] == 4) || ($_SESSION['userRole'] == 3) || ($_SESSION['userRole'] == 2) )
         require_once 'views/listbuttons.php';

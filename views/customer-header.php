6a80b8f0c8d8a493701e35038d87b24ced0ad258
mihassin <marko.hassinen@helsinki.fi>
2013-11-29 17:00:12 +0200

    perjantai

--- views/customer-header.php
+++ views/customer-header.php
@@ -4,7 +4,7 @@ echo "<!DOCTYPE html><html>
 <title>Pizzapalvelu</title></head>
 <body><nav>
 <a href='index.php'>Etusivu</a>
-<a href='makeorder.php'>Tee tilaus</a>
+<a href='cart.php'>Ostoskori</a>
 <a href='logout.php'>Kirjaudu ulos</a>
 </nav>";
 ?>

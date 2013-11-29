43f507ab2750af49588c42b49b168de6e2fb35b4
mihassin <marko.hassinen@helsinki.fi>
2013-11-29 18:06:37 +0200

    perjantai

--- remove-itemCheck.php
+++ remove-itemCheck.php
@@ -3,6 +3,11 @@ require_once 'libs/checkAccess.php';
 require_once 'libs/db_connect.php';
 $name = pg_escape_string(getConnection(), $_POST['productlist']);
 $pid = $_SESSION['pid'];
-$sql = "DELETE FROM products WHERE product_type_id='{$pid}' AND name='{$name}';";
+
+if($pid == 3)
+ $sql = "DELETE FROM materials WHERE description='{$name}';";
+else
+ $sql = "DELETE FROM products WHERE product_type_id='{$pid}' AND name='{$name}';";
+
 pg_query(getConnection(), $sql);
 header('Location: http://mihassin.users.cs.helsinki.fi/');?>

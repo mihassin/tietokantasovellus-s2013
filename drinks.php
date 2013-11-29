e3159bd8866690f5b1ff9ee716b8b2e591a2fbef
mihassin <marko.hassinen@helsinki.fi>
2013-11-29 17:14:08 +0200

    perjantai

--- drinks.php
+++ drinks.php
@@ -4,5 +4,8 @@
       require_once 'views/drinks.php'; 
       require_once 'libs/listing.php'; 
       getList($kysely);
+      $kysely = "SELECT name FROM products WHERE product_type_id='2';";
+      if( ($_SESSION['userRole'] == 4) || ($_SESSION['userRole'] == 3) || ($_SESSION['userRole'] == 2) )
+       require_once 'views/listbuttons.php';
       require_once 'views/footer.php'; 
 ?>

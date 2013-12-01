fbfe690adb1e771e68c50e0e689fbe85fecc8a1c
mihassin <marko.hassinen@helsinki.fi>
2013-12-01 13:21:14 +0200

    sunnuntai

--- cart.php
+++ cart.php
@@ -4,6 +4,11 @@
       require_once 'views/header.php';
       echo "<h1>Ostoskori</h1>";
       require_once 'libs/list-cart.php';
-      getCartList($kysely);
+      $uid = $_SESSION['userId'];
+      $kysely = "SELECT products.name as tuote, cart_map.amount as kpl, products.price as price" 
+      $kysely .= " FROM products, cart_map" 
+      $kysely .= " WHERE products.id = cart_map.product_id AND cart_map.ordered = FALSE AND cart_map.user_id = '{$uid}';";
+      echo $kysely;
+      //getCartList($kysely);
       require_once 'views/cartbuttons.php';
       require_once 'views/footer.php'; ?>

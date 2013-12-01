ff8d019d991eb0c02583fea467034c6faa98bd22
mihassin <marko.hassinen@helsinki.fi>
2013-12-01 13:00:40 +0200

    sunnuntai

--- cart.php
+++ cart.php
@@ -1,7 +1,7 @@
 <?php session_start();
-      if(!isset($_SESSION['userId'])
+      if(!isset($_SESSION['userId']))
        header('Location: http://mihassin.users.cs.helsinki.fi'); 
-      require_once 'header.php';
+      require_once 'views/header.php';
       echo "<h1>Ostoskori</h1>";
       require_once 'views/cartbuttons.php';
-      require_once 'footer.php'; ?>
+      require_once 'views/footer.php'; ?>

a745bc0d442bda90dbe070827e5dafb9f59eb2b9
mihassin <marko.hassinen@helsinki.fi>
2013-12-01 13:16:31 +0200

    sunnuntai

--- cart.php
+++ cart.php
@@ -3,5 +3,7 @@
        header('Location: http://mihassin.users.cs.helsinki.fi'); 
       require_once 'views/header.php';
       echo "<h1>Ostoskori</h1>";
+      require_once 'libs/list-cart.php';
+      getCartList($kysely);
       require_once 'views/cartbuttons.php';
       require_once 'views/footer.php'; ?>

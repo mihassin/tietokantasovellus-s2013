63c271c5d13e295fc63206127db356775da59a8c
mihassin <marko.hassinen@helsinki.fi>
2013-12-05 13:06:53 +0200

    korjauksia

--- add-itemCheck.php
+++ add-itemCheck.php
@@ -1,18 +1,25 @@
 <?php
 if(!is_numeric($_POST['price'])) {
  header('Location: http://mihassin.users.cs.helsinki.fi/add-offer.php');
+ exit();
 }
+
 require_once 'libs/checkAccess.php';
 require_once 'libs/db_connect.php';
 $yhteys = getConnection();
+
+$price = pg_escape_string($yhteys, $_POST['price']);
+if(price <= 0) {
+ header('Location: http://mihassin.users.cs.helsinki.fi/add-offer.php');
+ exit(); 
+}
 
 $str = pg_escape_string($yhteys, $_POST['product-type-list']);
 $query = pg_query($yhteys, "SELECT id FROM product_types WHERE description='{$str}';");
 $type = pg_fetch_result($query, 0, 0);
 
 $name = pg_escape_string($yhteys, $_POST['name']); //lisukkeiden description ja tuotteiden name
-$description = pg_escape_string($yhteys, $_POST['description']); //vain tuotteilla
-$price = pg_escape_string($yhteys, $_POST['price']); 
+$description = pg_escape_string($yhteys, $_POST['description']); //vain tuotteilla 
 
 if($_SESSION['pid']==3)
  $kysely = "INSERT INTO materials values (DEFAULT, {$type}, '{$name}', {$price});";
@@ -21,4 +28,6 @@ else
 
 pg_query($kysely);
 header('Location: http://mihassin.users.cs.helsinki.fi/');
+pg_close($yhteys);
+exit();
 ?>

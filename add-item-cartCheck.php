4580fc54adb718c5876c3e5fe0f54c2ac36cf034
mihassin <marko.hassinen@helsinki.fi>
2013-12-05 13:00:36 +0200

    korjauksia

--- add-item-cartCheck.php
+++ add-item-cartCheck.php
@@ -2,10 +2,17 @@
 
 if(!is_numeric($_POST['amount'])) {
  header('Location: http://mihassin.users.cs.helsinki.fi/cart.php');
+ exit();
 }
 
 require_once 'libs/db_connect.php';
 $yhteys = getConnection();
+
+$amount = pg_escape_string($yhteys, $_POST['amount']); // määrä
+if($amount <= 0) {
+ header('Location: http://mihassin.users.cs.helsinki.fi/cart.php');
+ exit();
+}
 
 $str = pg_escape_string($yhteys, $_POST['productlist']);
 $query = pg_query($yhteys, "SELECT id FROM products WHERE name='{$str}';");
@@ -13,10 +20,10 @@ $pid = pg_fetch_result($query, 0, 0); //product id
 
 $uid = $_SESSION['userId']; // user id
 
-$amount = pg_escape_string($yhteys, $_POST['amount']); // määrä
-
 $kysely = "INSERT INTO cart_map VALUES (DEFAULT, {$uid}, {$pid}, {$amount}, FALSE);"; 
 
 pg_query($kysely);
 header('Location: http://mihassin.users.cs.helsinki.fi/cart.php');
+pg_close($yhteys);
+exit();
 ?>

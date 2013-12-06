433ab8e0ad99e0eaf9c79041d46ef3feaa2aea0a
mihassin <marko.hassinen@helsinki.fi>
2013-12-06 18:46:31 +0200

    perjantai

--- add-drink-cartCheck.php
+++ add-drink-cartCheck.php
@@ -2,21 +2,29 @@
 
 if(!is_numeric($_POST['amount'])) {
  header('Location: http://mihassin.users.cs.helsinki.fi/cart.php');
+ exit();
 }
 
 require_once 'libs/db_connect.php';
 $yhteys = getConnection();
 
+$amount = pg_escape_string($yhteys, $_POST['amount']); // määrä
+if($amount <= 0) {
+ header('Location: http://mihassin.users.cs.helsinki.fi/cart.php');
+ exit();
+}
+
 $str = pg_escape_string($yhteys, $_POST['drinklist']);
-$query = pg_query($yhteys, "SELECT id FROM products WHERE name='{$str}';");
+$query = pg_query($yhteys, "SELECT id, price FROM products WHERE name='{$str}';");
 $pid = pg_fetch_result($query, 0, 0); //product id
+$tot_price = $amount * pg_fetch_result($query, 0, 1);
 
 $uid = $_SESSION['userId']; // user id
 
-$amount = pg_escape_string($yhteys, $_POST['amount']); // määrä
-
-$kysely = "INSERT INTO cart_map VALUES (DEFAULT, {$uid}, {$pid}, {$amount}, FALSE);"; 
+$kysely = "INSERT INTO cart_map VALUES (DEFAULT, {$uid}, {$pid}, {$amount}, FALSE, '',{$tot_price});"; 
 
 pg_query($kysely);
 header('Location: http://mihassin.users.cs.helsinki.fi/cart.php');
+pg_close($yhteys);
+exit();
 ?>

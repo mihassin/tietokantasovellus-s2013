4de568b695ac9773cea4c1c86ad7e1d06970ed0e
mihassin <marko.hassinen@helsinki.fi>
2013-12-06 17:05:53 +0200

    perjantai

--- order-check.php
+++ order-check.php
@@ -11,8 +11,9 @@ $time = pg_escape_string($yhteys, $_POST['deliver']);
 $tot_price = getTotal($_SESSION['userId']);
 
 $kysely = "INSERT INTO orders VALUES (DEFAULT, {$uid}, '{$addr}','{$time}',{$tot_price});";
-echo $kysely;
+pg_query($yhteys, $kysely);
 
+$kysely = "UPDATE cart_map SET ordered=TRUE WHERE user_id={$uid} AND ordered=FALSE;";
 pg_query($yhteys, $kysely);
 pg_close($yhteys);
 header('Location: http://mihassin.users.cs.helsinki.fi/');

222bb39d3793b01cffeb72568f59b058e20edb30
mihassin <marko.hassinen@helsinki.fi>
2013-12-06 18:04:21 +0200

    perjantai

--- views/order.php
+++ views/order.php
@@ -1,9 +1,10 @@
 <?php require_once 'header.php' ?>
  <h1>Tilaus</h1>
+ <p>* vaaditut kentät.</p>
  <form method='post' action='order-check.php'>
-  <label>Osoite</label></br>
+  <label>Osoite</label> *</br>
   <input type='text' name='address' /></br>
-  <label>Toimitus aika</label></br>
+  <label>Toimitus aika</label> *</br>
   <input type='text' name='deliver' /></br></br>
   Tilauksen yhteishinta on <?php echo $tot_price . " yksikköä.</br></br>"; ?>
   <input type='submit' value='Lähetä tilaus' />

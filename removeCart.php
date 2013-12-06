e00ea99725be7918014e97c75806b2d36de9f628
mihassin <marko.hassinen@helsinki.fi>
2013-12-06 19:35:28 +0200

    perjantai

---
+++ removeCart.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'libs/db_connect.php';
+$uid = $_SESSION['userId'];
+$yhteys = getConnection();
+$kysely = "DELETE FROM cart_map WHERE user_id={$uid} AND ordered=FALSE";
+pg_query($yhteys, $kysely);
+pg_close($yhteys);
+header('Location: http://mihassin.users.cs.helsinki.fi/cart.php');
+exit();
+?>

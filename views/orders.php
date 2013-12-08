dede7639a9c8cf1cf9e7adad831674efee8ce3cf
mihassin <marko.hassinen@helsinki.fi>
2013-12-08 19:02:32 +0200

    vikat

---
+++ views/orders.php
@@ -0,0 +1,6 @@
+<form method='post' action='deliver.php'>
+ <input type='submit' value='Merkitse toimitetuksi' />
+</form>
+<form method='post' action='remove-delivered.php>
+ <input type='submit' value='Poista toimitetut' />
+</form>

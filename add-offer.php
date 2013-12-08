adbc678cc7b0fa882bf003f112bd53ec26a9c5ff
mihassin <marko.hassinen@helsinki.fi>
2013-12-08 15:35:52 +0200

    vikat

--- add-offer.php
+++ add-offer.php
@@ -3,8 +3,8 @@ require_once 'libs/checkAccess.php';
 require_once 'libs/dropList.php';
 $pid = $_SESSION['pid'];
 if($pid == 3)
- $kysely = "SELECT description as name FROM materials";
+ $kysely = "SELECT id, description as name FROM materials";
 else
- $kysely = "SELECT name FROM products WHERE product_type_id='{$pid}'";
+ $kysely = "SELECT id, name FROM products WHERE product_type_id='{$pid}'";
 
 require_once 'views/add-offer.php'; ?>

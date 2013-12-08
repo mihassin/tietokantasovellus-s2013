2e7a7ecdb87203593d8e54f401161c0aa7aec7db
mihassin <marko.hassinen@helsinki.fi>
2013-12-08 17:21:39 +0200

    vikat

--- order-check.php
+++ order-check.php
@@ -7,12 +7,19 @@ $yhteys = getConnection();
 $uid = $_SESSION['userId'];
 
 $addr = pg_escape_string($yhteys, $_POST['address']);
+
+if(empty($addr) || (strlen($addr) < 6)) {
+	pg_close($yhteys);
+	header('Location: http://mihassin.users.cs.helsinki.fi/order.php');
+	exit();
+}
+
 $addr = checkData($addr);
 $time = date("Y-m-d H:i:s");
 
 $tot_price = getTotal($_SESSION['userId']);
 
-$kysely = "INSERT INTO orders VALUES (DEFAULT, {$uid}, '{$addr}','{$time}', FALSE,{$tot_price});";
+$kysely = "INSERT INTO orders VALUES (DEFAULT, {$uid}, '{$addr}','{$time}', FALSE ,{$tot_price});";
 pg_query($yhteys, $kysely);
 
 $kysely = "UPDATE cart_map SET ordered=TRUE WHERE user_id={$uid} AND ordered=FALSE;";
